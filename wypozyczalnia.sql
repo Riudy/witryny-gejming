91df39b5902f94d7dac15e963987ccf2c60e26e6
Riudy <90309086+Riudy@users.noreply.github.com>
2024-10-07 13:34:28 +0200

    Add files via upload

---
+++ wypozyczalnia.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Paź 03, 2024 at 09:17 AM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wypozyczalnia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `autorzy`
+--
+
+CREATE TABLE `autorzy` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(30) NOT NULL,
+  `nazwisko` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `klienci`
+--
+
+CREATE TABLE `klienci` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(20) NOT NULL,
+  `nazwisko` varchar(20) NOT NULL,
+  `adres` varchar(40) NOT NULL,
+  `data_zalozenia` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ksiazki`
+--
+
+CREATE TABLE `ksiazki` (
+  `id` int(11) NOT NULL,
+  `tytul` varchar(50) NOT NULL,
+  `gatunek` varchar(25) NOT NULL,
+  `autor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wypozyczenia`
+--
+
+CREATE TABLE `wypozyczenia` (
+  `id` int(11) NOT NULL,
+  `id_ksiazki` int(11) NOT NULL,
+  `id_klienta` int(11) NOT NULL,
+  `data_od` date NOT NULL,
+  `data_do` date NOT NULL,
+  `zwrocono` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `autorzy`
+--
+ALTER TABLE `autorzy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `autor` (`autor`);
+
+--
+-- Indeksy dla tabeli `wypozyczenia`
+--
+ALTER TABLE `wypozyczenia`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_ksiazki` (`id_ksiazki`),
+  ADD KEY `id_klienta` (`id_klienta`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `autorzy`
+--
+ALTER TABLE `autorzy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `klienci`
+--
+ALTER TABLE `klienci`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `wypozyczenia`
+--
+ALTER TABLE `wypozyczenia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  ADD CONSTRAINT `ksiazki_ibfk_1` FOREIGN KEY (`autor`) REFERENCES `autorzy` (`id`);
+
+--
+-- Constraints for table `wypozyczenia`
+--
+ALTER TABLE `wypozyczenia`
+  ADD CONSTRAINT `wypozyczenia_ibfk_1` FOREIGN KEY (`id_ksiazki`) REFERENCES `ksiazki` (`id`),
+  ADD CONSTRAINT `wypozyczenia_ibfk_2` FOREIGN KEY (`id_klienta`) REFERENCES `klienci` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
